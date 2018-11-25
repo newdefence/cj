c69cc9020d21b8368b288c282e0c888ef620b6ea
guofang.zhang <newdefence@163.com>
2018-11-26 01:19:23 +0800

    格式化文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,20 @@
 // https://github.com/prettier/eslint-config-prettier
 
 module.exports = {
-  extends: ['eslint:recommended', 'react-app', 'plugin:prettier/recommended'],
-  rules: {
-    eqeqeq: [2, 'smart'],
-    'max-len': [1, { code: 240, ignoreUrls: true }],
-    quotes: [2, 'single'],
-    'react/jsx-max-props-per-line': [1, { maximun: 10, when: 'multiline' }]
-  }
+    extends: ['eslint:recommended', 'react-app'],
+    rules: {
+        'comma-dangle': [2, {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
+        eqeqeq: [2, 'smart'],
+        indent: [2, 4],
+        'max-len': [1, { code: 240, ignoreUrls: true }],
+        'no-console': 0,
+        quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+        'react/jsx-max-props-per-line': [1, { maximun: 10, when: 'multiline' }]
+    }
 };
